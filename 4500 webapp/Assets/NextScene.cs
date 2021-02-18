4db6f7cc1f8d5cd8145f7b33f26bbca3612dd1db
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-02-17 20:32:18 -0800

    Scene switching unity game

---
+++ 4500 webapp/Assets/NextScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
+public class NextScene : MonoBehaviour
+{
+    public string sceneName; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       // sceneName = "2nd Scene";
+    }
+    public void buttonBehavior()
+    {
+        Debug.Log("Clicked"); 
+        SceneManager.LoadScene(sceneName);   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
